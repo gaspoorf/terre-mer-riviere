ee84fe8ddb03c4d8aea941385cb9b4a130d6e782
Gaspard <gaspard_hedde@yahoo.com>
2025-07-09 22:11:03 +0200

    1st try scroll

--- src/scripts/lenis-init.js
+++ src/scripts/lenis-init.js
@@ -1,8 +1,28 @@
 import Lenis from 'lenis';
 
-const lenis = new Lenis();
+const lenis = new Lenis({
+    // duration: 1.2,
+    // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    // smooth: true,
+    // smoothTouch: true,
+    // touchMultiplier: 1.5,
+
+    direction: "vertical",
+    gestureDirection: "vertical",
+    smooth: true,
+    smoothTouch: true,
+    touchMultiplier: 1.5,
+    infinite: false,
+    autoResize: true,
+
+});
+  
+
+
+
 function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
+    lenis.raf(time);
+    requestAnimationFrame(raf);
 }
+
 requestAnimationFrame(raf);
