c5f5a62619b999a4af834c6dec61dab2c4522504
Gaspard <gaspard_hedde@yahoo.com>
2025-07-16 22:54:52 +0200

    FIX: responsive + animations

--- src/content/home/index.md
+++ src/content/home/index.md
@@ -1,12 +1,12 @@
 ---
 title: Page d'accueil
 first:
-  - title: Terre<span> ⸱</span> Mer <span>⸱ </span>Rivière
+  - title: Terre <span> ⸱ </span> Mer <span> ⸱ </span> Rivière
     image: /ile-crop.png
     text: ' L’association des amis du pays de l’Aven et du Bélon, attentive à son environnement.'
 blocks:
   - title: Ria
-    svg: /icon-ria.svg
+    svg: /Group 407.svg
     image: /riviere.jpeg
     text: >-
       Sauvegarder les milieux aquatiques des cours d’eaux Sauvegarder les
@@ -14,7 +14,7 @@ blocks:
     color: '#74A6AC'
     lines: 1
   - title: Mer
-    svg: /icon-sea.svg
+    svg: /icon-sea-2.svg
     image: /vagues.png
     text: >-
       Agir pour la protection de l’environment marin et du littoral Agir pour la
@@ -22,10 +22,10 @@ blocks:
     color: '#0B2828'
     lines: 2
   - title: Terre
-    svg: /icon-earth.svg
+    svg: /Group 406.svg
     image: /terre.png
     text: >-
-      Valorisation des sites naturels, urbains et historiques. Préservation et
+      valorisation des sites naturels, urbains et historiques. Préservation et
       aménagements raisonnés du territoire
     color: '#7A8450'
     lines: 3
--- src/scripts/animations.js
+++ src/scripts/animations.js
@@ -1,57 +1,151 @@
-// animations.js
 import gsap from 'gsap';
 import ScrollTrigger from "gsap/ScrollTrigger";
 
-// Enregistrer le plugin
+import SplitType from "split-type";
 gsap.registerPlugin(ScrollTrigger);
 
 
 export function animateTitleContent() {
-    gsap.fromTo('.title-content', 
-    {
-        opacity: 0,
-    },
-    {
+    // gsap.fromTo('.title-content', 
+    // {
+    //     opacity: 0,
+    // },
+    // {
+    //     opacity: 1,
+    //     duration: 1.2,
+    //     delay: 1,
+    //     ease: "power1.inOut",
+    //     }
+    // );
+
+    const titleWords = document.querySelectorAll(".title-split");
+
+    gsap.set(titleWords, {
         opacity: 1,
-        duration: 1.2,
-        ease: "power1.inOut",
-        }
-    );
+    });
+    const splitedIntro = new SplitType(titleWords, {
+        types: "words, chars",
+    });
+
+    var charDelay = 1; 
+    splitedIntro.chars.forEach((char) => {
+        gsap.to(char, {
+            opacity: 1,
+            filter: "blur(0px)",
+            delay: charDelay,
+        });
+        charDelay = charDelay + 0.05;
+    });
+
+
+
+    const textWords = document.querySelectorAll(".text-split");
+    const splitedText = new SplitType(textWords, {
+        types: "words, chars",
+    });
+
+    var textDelay = 0; 
+    splitedText.chars.forEach((char) => {
+        gsap.set(char, {
+            opacity: 0,
+            filter: "blur(0px)",
+        });
+
+        gsap.to(char, {
+            opacity: 1,
+            filter: "blur(0px)",
+            delay: textDelay,
+
+            scrollTrigger: {
+                trigger: textWords,
+                start: 'top center+=20%',
+                end: 'bottom center',
+            }
+        });
+        textDelay = textDelay + 0.02;
+    });
+
+    
+    
+    const contentWords = document.querySelectorAll(".content-split");
+    contentWords.forEach((contentElement, index) => {
+        const splitedContentText = new SplitType(contentElement, {
+            types: "words",
+        });
+        
+        let textDelay = 0;
+        splitedContentText.words.forEach((word) => {
+            gsap.set(word, {
+                opacity: 0,
+                filter: "blur(10px)",
+                x: 100,
+            });
+            gsap.to(word, {
+                opacity: 1,
+                filter: "blur(0px)",
+                x: 0,
+                ease: "power1.inOut",
+                duration: 0.5,
+                delay: textDelay,
+                scrollTrigger: {
+                    trigger: contentElement,
+                    start: 'top center+=20%',
+                    end: 'bottom center',
+                }
+            });
+            textDelay = textDelay + 0.02;
+        });
+    });
+
+
+
+
+
 
     gsap.fromTo('.cloud-1', 
-    {
-        y: 0,
-    },
-    {
-        y: -50,
-
-        ease: "power1.inOut",
-        scrub: true,
-        scrollTrigger: {
-            trigger: '.cloud-1',
-            start: 'top center',
-            end: 'bottom bottom',
-            scrub: true,
+        { y: 50 },
+        {
+            y: -100,
+            ease: "power1.inOut",
+            scrollTrigger: {
+                trigger: '.cloud-1',
+                start: 'top bottom',
+                end: 'bottom top',
+                scrub: true,
+                invalidateOnRefresh: true,
+            }
         }
-    }
     );
 
-    gsap.fromTo('.cloud-2', 
-    {
-        y: -100,
-    },
-    {
-        y: 100,
-      
-        ease: "power1.inOut",
-        scrub: true,
-        scrollTrigger: {
-            trigger: '.cloud-2',
-            start: 'top-=100% center',
-            end: 'bottom+=100% center',
-            scrub: true,
+
+
+    
+    const cloud2 = document.querySelector('.cloud-2');
+    gsap.fromTo(cloud2,
+        {
+            y: 100,
+            opacity: 0,
+        },
+        {
+            y: 0,
+            opacity: 1,
+            ease: "power2.out",
+            duration: 2,
+            onComplete: () => {
+                gsap.to(cloud2, {
+                    y: 200,
+                    ease: "power1.inOut",
+                    immediateRender: false,
+                    scrollTrigger: {
+                        trigger: cloud2,
+                        start: 'top bottom',
+                        end: 'bottom top',
+                        scrub: true,
+                        invalidateOnRefresh: true,
+                    }
+                });
+            }
         }
-    }
     );
 
 
